5fc8c453a032231263dbbadcfa2d11f782895481
stumburs <stumburs@inbox.lv>
2022-07-28 23:14:28 +0300

    Added RGB to HEX snippet. Updated footer.

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 document.write('<div class="footer">');
 document.write(
-    '<p class="lead text-center footer-text">Updated: 26 Jul 2022</p>'
+    '<p class="lead text-center footer-text">Updated: 28 Jul 2022</p>'
 );
 document.write("</div>");
