437515da5c18d42ebfbe2f3fe0482d2bb69a64ac
stumburs <stumburs@inbox.lv>
2022-08-03 02:27:08 +0300

    Updated footer.

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 document.write('<div class="footer">');
 document.write(
-    '<p class="lead text-center footer-text">Updated: 02 Aug 2022</p>'
+    '<p class="lead text-center footer-text">Updated: 03 Aug 2022</p>'
 );
 document.write("</div>");
