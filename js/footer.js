73269bce3f8e2b024eaa8607cc0de2fa279555f3
stumburs <stumburs@inbox.lv>
2022-12-26 03:22:19 +0200

    Added SCP-087, removed flowfield.

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,3 @@
 document.write('<div class="footer">');
-document.write('<p class="lead text-center footer-text">Updated: 10 Nov 2022</p>');
+document.write('<p class="lead text-center footer-text">Updated: 26 Dec 2022</p>');
 document.write("</div>");
