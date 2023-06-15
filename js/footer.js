1b4c24623af9f359d2f32c075cefec6f2a091fb4
stumburs <stumburs@inbox.lv>
2023-06-16 00:05:05 +0300

    Added particle system

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 document.write('<div class="footer">');
 document.write(
-    '<p class="lead text-center footer-text">Updated: 09 Apr 2023</p>'
+    '<p class="lead text-center footer-text">Updated: 16 Jun 2023</p>'
 );
 document.write("</div>");
